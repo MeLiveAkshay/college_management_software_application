c2e945221e6b0b6c0cbb0f7ea2440df5f8ca790d
MeLiveAkshay <akshaykumarprajapati@hotmail.com>
2025-03-02 11:52:32 +0530

    college management system software

---
+++ android/app/src/main/kotlin/com/example/college_management_sytem_software/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.college_management_sytem_software
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/home_screen.dart
@@ -0,0 +1,18 @@
+
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Flutter Demo Home Page'),
+      ),
+      body: Center(
+        child: Text('Hello World'),
+      ),
+    );
+  }
+}
